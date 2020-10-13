1d1db8c7db06d0aafb1aa936297966bac1c95b09
luzixiaoneu <lu.zix@northeastern.edu>
2020-10-13 13:40:03 -0400

    first commit

---
+++ PPMToolFullStack/src/test/java/ppmtool/personalproject/PersonalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ppmtool.personalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
